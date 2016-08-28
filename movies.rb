dfa017dba9609fc99d3e38e33397894854132fa9
Vivek Bhat <bhatvivek93@gmail.com>
2016-08-28 00:26:51 -0400

    first commit

---
+++ movies.rb
@@ -0,0 +1,8 @@
+movies = {}
+
+puts "What would you like to do?"
+puts "-- Type 'add' to add a movie."
+puts "-- Type 'update' to update a movie."
+puts "-- Type 'display' to display all movies."
+puts "-- Type 'delete' to delete a movie."
+
