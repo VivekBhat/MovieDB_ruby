5a32d903c3f274401b3e557a46f00f2360c8b577
Vivek Bhat <bhatvivek93@gmail.com>
2016-08-28 00:30:17 -0400

    Added functions

--- movies.rb
+++ movies.rb
@@ -6,3 +6,30 @@ puts "-- Type 'update' to update a movie."
 puts "-- Type 'display' to display all movies."
 puts "-- Type 'delete' to delete a movie."
 
+choice = gets.chomp
+
+case choice
+  when "add"
+    puts "Enter Movie: "
+    title =  gets.chomp.to_sym
+    puts "Enter Rating: "
+    rating = gets.chomp.to_i
+    movies[title] = rating
+
+  when "update"
+    puts "Updated!"
+
+  when "diplay"
+    puts "Movies!"
+
+  when "delete"
+    puts "Deleted!"
+
+  else
+    puts"Error!"
+
+end
+
+movies.each do | a , b |
+  puts  "Movie: #{a} got #{b} rating"
+end
